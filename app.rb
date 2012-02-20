a96fbf61ec71369294fb20b31108c56d8c56e7d0
Matthew Strickland <mpstrick@gmail.com>
2012-02-20 12:39:14 -0500

    Add pages for each session as well as a main session page if necessary?

--- app.rb
+++ app.rb
@@ -10,6 +10,26 @@ get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
+get '/session/one' do
+  File.read(File.join('public', 'one.html'))
+end
+
+get '/sessions' do
+  File.read(File.join('public', 'sessions.html'))
+end
+
+get '/session/two' do
+  File.read(File.join('public', 'two.html'))
+end
+
+get '/session/three' do
+  File.read(File.join('public', 'three.html'))
+end
+
+get '/session/four' do
+  File.read(File.join('public', 'four.html'))
+end
+
 not_found do
   'The item you are looking for was not found :('
 end
