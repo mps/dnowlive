3a10753d7dd69a14d9f81b62c49ccfe666e8cc5a
Matthew Strickland <mpstrick@gmail.com>
2012-03-16 10:22:25 -0400

    add test url to preview site before going live.

--- app.rb
+++ app.rb
@@ -10,24 +10,8 @@ get '/' do
   File.read(File.join('public', 'index-offline.html'))
 end
 
-get '/sessions' do
-  File.read(File.join('public', 'sessions.html'))
-end
-
-get '/session/one' do
-  File.read(File.join('public', 'one.html'))
-end
-
-get '/session/two' do
-  File.read(File.join('public', 'two.html'))
-end
-
-get '/session/three' do
-  File.read(File.join('public', 'three.html'))
-end
-
-get '/session/four' do
-  File.read(File.join('public', 'four.html'))
+get '/test' do
+  File.read(File.join('public', 'index.html'))
 end
 
 not_found do
