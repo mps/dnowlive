e89f1f11e2ae0109ba611e1082836bf2339780dc
Matthew Strickland <mpstrick@gmail.com>
2012-03-19 21:35:32 -0400

    Setup site for offline mode with just the recap video.

--- app.rb
+++ app.rb
@@ -10,22 +10,6 @@ get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-get '/sessions/one' do
-  File.read(File.join('public', 'one.html'))
-end
-
-get '/sessions/two' do
-  File.read(File.join('public', 'two.html'))
-end
-
-get '/sessions/three' do
-  File.read(File.join('public', 'three.html'))
-end
-
-get '/sessions/four' do
-  File.read(File.join('public', 'four.html'))
-end
-
 not_found do
   'The item you are looking for was not found :('
 end
